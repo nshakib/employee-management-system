a3437991352fbc050b64084b848e14251c0aeba5
nshakib <nazmus.shakib.cse@gmail.com>
2024-09-18 16:44:10 +0600

    working with login system

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:5001/api/auth/login", 
+                {email,password});
+           if(response.data.success){
+            alert("Successfully logged in")
+           }
+        } catch (error) {
+            if(error.response && !error.response.data.success){
+                setError(error.response.data.error);
+            }  else {
+                setError("Server error");
+            }       
+        } 
+    }
   return (
     <div className='flex flex-col items-center h-screen justify-center
     bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6'>
@@ -12,19 +29,26 @@ const Login = () => {
         
         <div className='border shadow p-6 w-80 bg-white'>
             <h2 className='text-2xl font-bold mb-4'>Login</h2>
-            <form action="">
+            
+            {error && <p className='text-red-500'>{error}</p>}
+            
+            <form onSubmit={handleSubmit}>
                 <div className='mb-4'>
                     <label htmlFor='email' className='block text-gray-700'>Email</label>
                     <input type="email" 
                     className='w-full px-3 py-2 border' 
-                    placeholder='Enter Email' />
+                    placeholder='Enter Email' 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    required/>
                 </div>
                 <div className='mb-4'>
                     <label htmlFor='password' className='block text-gray-700'>Password</label>
                     <input 
                     type="password"
                     className='w-full px-3 py-2 border' 
-                    placeholder='******' />
+                    placeholder='******' 
+                    onChange={(e)=>setPassword(e.target.value)}
+                    required/>
                 </div>
 
                 <div className='mb-4 flex items-center justify-between'>
@@ -36,18 +60,17 @@ const Login = () => {
                         Forgot password?
                     </a>
                 </div>
-                <dib className="mb-4">
+                <div className="mb-4">
                     <button
-                    onClick={()=>handleSubmit}
                     type='submit' 
                     className='w-full bg-teal-600 text-white py-2'
                     >Login
                     </button>
-                </dib>
+                </div>
             </form>
         </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './routes/auth.js';
+import connectToDatabase from './db/db.js';
 
-
-const app = express();
+connectToDatabase();
+const app = express()
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRouter)
 
 
 //server configuration
