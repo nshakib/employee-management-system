e41e7c7d3fb76c3080e183d96f7ec3a550cb02e5
nshakib <nazmus.shakib.cse@gmail.com>
2024-10-01 23:02:52 +0600

    working on employee section degin

--- frontend/src/components/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -14,8 +14,8 @@ const AdminSidebar = () => {
                 <FaTachometerAlt  />
                 <span>Dashboard</span>
             </NavLink>
-            <NavLink to="/admin-dashboard"
-            className="flex items-center space-x-4 block py-2.5 px-4 rounded">
+            <NavLink to="/admin-dashboard/employees"
+            className={({isActive})=> `${isActive ? "bg-teal-500 ": " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
                 <FaUsers  />
                 <span>Employee</span>
             </NavLink>
---
+++ frontend/src/utils/EmployeeHelper.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+export const fetchDepartments = async() => {
+      let departments
+      try {
+        const response = await axios.get("http://localhost:5001/api/department",{
+          headers:{
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        })
+
+        if (response.data.success){
+
+        }
+      } catch (error) {
+        if(error.response && !error.response.data.success)
+          {
+              alert(error.response.data.error)
+          }
+      }
+    }
